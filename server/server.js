b0f2bfb2d2e1dc04c93a1cc719ff05b1b0a19a4c
Hoa JP <itlmh2k3@gmail.com>
2025-10-02 16:39:22 +0700

    setup mongoDB

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./config/mongodb.js";
 
 const app = express();
 app.use(cors());
 
+await connectDB();
+
 // Route endpoint to check API Status
 app.get("/", (req, res) => {
-  res.send("API successfully connectedd");
+  res.send("API successfully connecteddd");
 });
 const port = process.env.PORT || 3000;
 
